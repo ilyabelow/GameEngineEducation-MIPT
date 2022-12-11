b2ada5558098a31b71e4422dc8b27041ff9c3118
ilyabelow <ilyabelow.almost.surely@gmail.com>
2022-12-12 01:31:58 +0300

    hotfix

--- FinalProject/SourceCode/GameEngine/EntitySystem/CreateEntityXML.h
+++ FinalProject/SourceCode/GameEngine/EntitySystem/CreateEntityXML.h
@@ -2,7 +2,7 @@
 
 #include <unordered_map>
 #include "flecs.h"
-
+#include <string>
 
 std::vector<float> vectorFromString(std::string string);
 flecs::entity createEntityFromXML(flecs::world& ecs, const char* filename);
